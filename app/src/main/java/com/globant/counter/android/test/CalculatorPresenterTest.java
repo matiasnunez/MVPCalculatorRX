d197ae5dcf3a5198b8a264163799af7b243d4f43
Matias Núñez <matias.nunez@globant.com>
2018-04-18 23:06:29 -0300

    Onboarding Module 2: Unit Tests Added Unit Tests Testing Done: Nexus 5x Signed-off-by: Matías Núñez matias.nunez@globant.com

---
+++ app/src/main/java/com/globant/counter/android/test/CalculatorPresenterTest.java
@@ -0,0 +1,26 @@
+package com.globant.counter.android.test;
+
+
+import com.globant.counter.android.mvp.model.CalculatorModel;
+import com.globant.counter.android.mvp.view.CalculatorView;
+
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+public class CalculatorPresenterTest {
+
+     /*
+
+*/
+
+
+}
